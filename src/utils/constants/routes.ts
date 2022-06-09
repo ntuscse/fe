7467c78baf443667e9e913926e1b54a870739d83
Woke Orange <75624491+wokeorange@users.noreply.github.com>
2022-06-09 21:25:56 +0800

    Typings for Voucher & Cart

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -1,19 +1,25 @@
 type Routes = {
-    HOME: string
-    SIGN_UP: string
-    CONFIRM_SIGN_UP: string
-    SIGN_IN: string
-    FORGOT_PASSWORD: string
-    MERCHANDISE_LIST: string
-}
+  HOME: string;
+  SIGN_UP: string;
+  CONFIRM_SIGN_UP: string;
+  SIGN_IN: string;
+  FORGOT_PASSWORD: string;
+  MERCHANDISE_LIST: string;
+  CART: string;
+  CHECKOUT: string;
+  ORDER_SUMMARY: string;
+};
 
 const routes: Routes = {
-    HOME: '/',
-    SIGN_UP: '/sign-up',
-    CONFIRM_SIGN_UP: '/confirm-sign-up',
-    SIGN_IN: '/sign-in',
-    FORGOT_PASSWORD: '/forgot-password',
-    MERCHANDISE_LIST: '/merchandise-list'
-}
+  HOME: "/",
+  SIGN_UP: "/sign-up",
+  CONFIRM_SIGN_UP: "/confirm-sign-up",
+  SIGN_IN: "/sign-in",
+  FORGOT_PASSWORD: "/forgot-password",
+  MERCHANDISE_LIST: "/merchandise-list",
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  ORDER_SUMMARY: "/summary",
+};
 
 export default routes;
