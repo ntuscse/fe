14c127bebfba8cd300dfb1d722b8f1a49706d736
Woke Orange <75624491+wokeorange@users.noreply.github.com>
2022-06-09 21:30:10 +0800

    Restructure Header & Footer
    Place Header & Footer outside, prevent importing in every page.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,13 @@
-import Header from "../../components/Header";
 import HeroCarousel from "./HeroCarousel";
 import RecentPosts from "./RecentPosts";
 import RecentEvents from "./RecentEvents";
-import Footer from "../../components/Footer";
 
 export const Home = () => {
   return (
     <div>
-      <Header />
       <HeroCarousel />
       <RecentPosts />
       <RecentEvents />
-      <Footer />
     </div>
   );
 };
