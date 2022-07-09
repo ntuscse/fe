ea376e4ab067aded0a2037bf0d57bfe97de709e0
Dyllon <dyllon.g@outlook.com>
2022-07-09 21:42:52 +0800

    Cleanup import

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import ConfirmSignUp from "./pages/ConfirmSignUp";
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import { MerchandiseList } from "./pages/MerchandiseList/MerchandiseList";
+import MerchandiseList  from "./pages/MerchandiseList";
 import routes from "./utils/constants/routes";
 
 const Routes = () => (
