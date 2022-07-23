69aaac7d4f1232feb2da98a246ef0be9b19cf854
Dyllon <dyllon.g@outlook.com>
2022-07-23 12:10:17 +0800

    Add Page Component

---
+++ src/components/Page.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import { Flex, FlexProps } from "@chakra-ui/react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+type PageProps = FlexProps & {
+    children: ReactNode
+    hideHeader?: boolean
+}
+
+const Page = ({ children, hideHeader = false, ...props }: PageProps) => {
+    return <Flex flexDirection="column" {...props}>
+        {!hideHeader && <Header/>}
+        {children}
+        <Footer/>
+    </Flex>
+}
+
+export default Page
