35e617e02927e5da1e9bed5d502f651c0628c471
Dyllon <dyllon.g@outlook.com>
2022-07-23 12:15:35 +0800

    Lint fix

--- src/pages/MerchandiseList/Card.tsx
+++ src/pages/MerchandiseList/Card.tsx
@@ -12,13 +12,13 @@ type card = {
     sizeRange: string;
 }
 
-const Card = ( {imgSrc, text, price, sizeRange}:card ) => {
+const Card = ( { imgSrc, text, price, sizeRange }:card ) => {
     return (
         <Box p={3} w={{ base: 190, lg: '24%' }} h={[250, '100%']} >
             <Box boxShadow='md' borderRadius={5}><Image src={imgSrc} h={{ base: 200, lg: '20%' }} borderRadius={5}/></Box>
             <Flex justifyContent='space-between'>
                 <Text visibility='hidden'>{sizeRange}</Text>
-                <a href="/" style={{textDecoration : 'none'}}><Text align='center' textStyle={['sm', 'h6']} textColor='primary.600' fontWeight={700}>{text}</Text></a>
+                <a href="/" style={{ textDecoration : 'none' }}><Text align='center' textStyle={['sm', 'h6']} textColor='primary.600' fontWeight={700}>{text}</Text></a>
                 <Text textColor='gray.400' fontWeight={700} fontSize={['sm', 'lg']}>{sizeRange}</Text>
             </Flex>
             <Text align='center' fontSize={['sm', 'md']} textColor='primary.600'>{price}</Text>
