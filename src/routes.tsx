f04be85e96c46b40d5dd7d537a9ed6275c55f91b
nicolelst <nicolelim2608@gmail.com>
2022-11-11 16:33:10 -0500

    fix: make merchandise list route `/`
    This commit removes routes to the old home page (pages/Home) and makes the merchandise list page(pages/MerchandiseList) the main landing page.
    
    [SCSE-53]

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes as ReactRoutes } from "react-router-dom";
 import ConfirmSignUp from "./pages/ConfirmSignUp/ConfirmSignUp";
-import Home from "./pages/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/Signup/SignUp";
 import MerchandiseList from "./pages/MerchandiseList";
@@ -14,16 +13,15 @@ import Error404 from "./pages/Error404";
 
 const Routes = () => (
   <ReactRoutes>
-    <Route path={routes.HOME} element={<Home />} />
+    <Route path={routes.HOME} element={<MerchandiseList />} />
     <Route path={routes.SIGN_IN} element={<SignIn />} />
     <Route path={routes.SIGN_UP} element={<SignUp />} />
     <Route path={routes.CONFIRM_SIGN_UP} element={<ConfirmSignUp />} />
-    <Route path={routes.MERCHANDISE_LIST} element={<MerchandiseList />} />
     <Route path={routes.CART} element={<Cart />} />
     <Route path={routes.CHECKOUT} element={<Checkout />} />
     <Route path={`${routes.ORDER_SUMMARY}/:slug`} element={<OrderSummary />} />
     <Route path={routes.ORDER_HISTORY} element={<OrderHistory />} />
-    <Route path={`${routes.MERCHANDISE_LIST}/:slug`} element={<MerchDetail />} />
+    <Route path={`${routes.HOME}/:slug`} element={<MerchDetail />} />
     <Route path="*" element={<Error404 />} />
   </ReactRoutes>
 );
