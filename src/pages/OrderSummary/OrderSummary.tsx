2bcf320594c964b807c9635dc98387ca4f9b3b64
kingsmil <23634290+kingsmil@users.noreply.github.com>
2023-01-27 06:48:20 +0800

    Update src/pages/OrderSummary/OrderSummary.tsx
    Co-authored-by: Chan Wen Xu <lutherchanpublic+git@gmail.com>

--- src/pages/OrderSummary/OrderSummary.tsx
+++ src/pages/OrderSummary/OrderSummary.tsx
@@ -98,7 +98,7 @@ export const OrderSummary: FC = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png"
         />
         <Text fontWeight="bold">Please bring this QR code to the SCSE Lounge to collect your order.</Text>
-        <Text>For any assistance, please contact our email address: merch@cse.com</Text>
+        <Text>For any assistance, please contact our email address: merch@ntuscse.com</Text>
       </Flex>
     </>
   );
