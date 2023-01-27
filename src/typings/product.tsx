891b9ee88f7791267410534d59b69a62139c5b58
nicolelst <nicolelim2608@gmail.com>
2023-01-28 01:57:56 +0800

    feat: added mock data for stock
    Stock for each ProductType is stored as a matrix with indices color,
    size.

--- src/typings/product.tsx
+++ src/typings/product.tsx
@@ -1,14 +1,18 @@
 // export type ProductSizeTypes = "3xs" | "xxs" | "xs" | "s" | "m" | "l" | "xl" | "2xl" | "3xl";
 
+import SizeDialog from "../pages/MerchDetail/SizeDialog";
+
 export type ProductCategoryType = string;
 
+export enum ProductTypeStockIndex {color, size};
 export type ProductType = {
   id: string;
   name: string;
   price: number;
+  stock: number[][]; // stock[color][size] = qty
+  sizes: string[];
+  colorways: string[];
   images?: string[];
-  sizes?: string[];
-  colorways?: string[];
   productCategory?: ProductCategoryType;
   isAvailable?: boolean;
 };
