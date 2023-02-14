e73ee0def36ce9c1b602d9a9646917ce39176897
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2023-02-14 10:49:56 +0800

    test structure

--- src/__tests__/MerchandiseList/Card.test.tsx
+++ src/__tests__/MerchandiseList/Card.test.tsx
@@ -1,1 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import CartHeader from "../../pages/Cart/CartHeader";
+
+describe("Card test", () => {
+	test("1 + 2 should return 3", () => {
+		render(<CartHeader />);
+		expect(1 + 2).toEqual(3);
+	});
+});
+
 export {};
