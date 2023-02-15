0a2fb597652e0eb847e6e95577b22af8334d7930
Dyllon <dyllon.g@outlook.com>
2023-02-15 23:49:18 +0800

    add products fixtures

---
+++ src/utils/fixtures/products.ts
@@ -0,0 +1,85 @@
+export const getProductsResponse = {
+    "products": [{
+        "id": "2022_005",
+        "name": "Graphic Stickers",
+        "price": 100,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_005_01.png", "https://cdn.ntuscse.com/merch/products/images/2022_005_02.png", "https://cdn.ntuscse.com/merch/products/images/2022_005_03.png"],
+        "sizes": ["Sticker"],
+        "colorways": ["Mascot", "Space", "SCSE"],
+        "productCategory": "Sticker",
+        "isAvailable": true,
+        "stock": [[0], [100], [2]]
+    }, {
+        "id": "2022_004",
+        "name": "Rainbow T-shirt",
+        "price": 1350,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_004_01.png", "https://cdn.ntuscse.com/merch/products/images/2022_004_02.png", "https://cdn.ntuscse.com/merch/products/images/2022_004_03.jpeg"],
+        "sizes": ["3XS", "2XS", "XS", "S", "M", "XL", "2XL", "3XL"],
+        "colorways": ["Rainbow"],
+        "productCategory": "T-shirt",
+        "isAvailable": true,
+        "stock": [[0, 1, 2, 3, 4, 3, 2, 1]]
+    }, {
+        "id": "2022_002",
+        "name": "Graphic T-shirt",
+        "price": 1300,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_002_01.png", "https://cdn.ntuscse.com/merch/products/images/2022_002_02.png", "https://cdn.ntuscse.com/merch/products/images/2022_002_03.png", "https://cdn.ntuscse.com/merch/products/images/2022_002_04.png"],
+        "sizes": ["3XS", "2XS", "XS", "S", "M", "XL", "2XL", "3XL"],
+        "colorways": ["Green", "Purple"],
+        "productCategory": "T-shirt",
+        "isAvailable": true,
+        "stock": [[0, 1, 2, 3, 4, 3, 2, 1], [1, 2, 3, 4, 5, 4, 3, 2]]
+    }, {
+        "id": "2022_007",
+        "name": "Lanyard",
+        "price": 450,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_007_01.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_007_02.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_007_03.jpeg"],
+        "sizes": ["Lanyard"],
+        "colorways": ["Green", "Purple", "Blue"],
+        "productCategory": "Lanyard",
+        "isAvailable": true,
+        "stock": [[100], [200], [100]]
+    }, {
+        "id": "2022_001",
+        "name": "SCSE Standard T-shirt",
+        "price": 1200,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_001_01.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_001_02.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_001_03.jpeg"],
+        "sizes": ["3XS", "2XS", "XS", "S", "M", "XL", "2XL", "3XL"],
+        "colorways": ["Black", "White", "Navy"],
+        "productCategory": "T-shirt",
+        "isAvailable": true,
+        "stock": [[0, 1, 2, 3, 4, 3, 2, 1], [1, 2, 3, 4, 5, 4, 3, 2], [0, 1, 2, 3, 4, 3, 2, 1]]
+    }, {
+        "id": "2022_006",
+        "name": "Hoodie Programmer",
+        "price": 2500,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_006_01.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_006_02.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_006_03.jpeg"],
+        "sizes": ["3XS", "2XS", "XS", "S", "M", "L", "XL", "2XL", "3XL"],
+        "colorways": ["Standard"],
+        "productCategory": "Hoodie",
+        "isAvailable": true,
+        "stock": [[0, 1, 2, 3, 4, 3, 2, 1]]
+    }, {
+        "id": "2022_003",
+        "name": "Dino T-shirt",
+        "price": 1200,
+        "images": ["https://cdn.ntuscse.com/merch/products/images/2022_003_01.png", "https://cdn.ntuscse.com/merch/products/images/2022_003_02.png", "https://cdn.ntuscse.com/merch/products/images/2022_003_03.png", "https://cdn.ntuscse.com/merch/products/images/2022_003_04.png"],
+        "sizes": ["3XS", "2XS", "XS", "S", "M", "XL", "2XL", "3XL"],
+        "colorways": ["Black", "White", "Navy", "Maroon"],
+        "productCategory": "T-shirt",
+        "isAvailable": true,
+        "stock": [[0, 1, 2, 3, 4, 3, 2, 1], [1, 2, 3, 4, 5, 4, 3, 2], [0, 1, 2, 3, 4, 3, 2, 1], [1, 2, 3, 4, 5, 4, 3, 2]]
+    }]
+}
+
+export const getProductResponse = {
+    "id": "2022_001",
+    "name": "SCSE Standard T-shirt",
+    "price": 1200,
+    "images": ["https://cdn.ntuscse.com/merch/products/images/2022_001_01.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_001_02.jpeg", "https://cdn.ntuscse.com/merch/products/images/2022_001_03.jpeg"],
+    "sizes": ["3XS", "2XS", "XS", "S", "M", "XL", "2XL", "3XL"],
+    "colorways": ["Black", "White", "Navy"],
+    "productCategory": "T-shirt",
+    "isAvailable": true,
+    "stock": [[0, 1, 2, 3, 4, 3, 2, 1], [1, 2, 3, 4, 5, 4, 3, 2], [0, 1, 2, 3, 4, 3, 2, 1]]
+}
