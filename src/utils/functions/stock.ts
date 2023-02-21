7bcce4913ff7089cc39152ce776df172fbf51a09
kingsmil <23634290+kingsmil@users.noreply.github.com>
2023-02-22 02:41:06 +0800

    Migrate stock implementation from number[][] to object, need further testing

--- src/utils/functions/stock.ts
+++ src/utils/functions/stock.ts
@@ -15,7 +15,7 @@ export const displayStock = (product: ProductType, colorway: string, size: strin
     const colorwayIndex = product.colorways.indexOf(colorway);
     const sizeIndex = product.sizes.indexOf(size);
     if (colorwayIndex !== -1 && sizeIndex !== -1) {
-        const qty = product.stock[colorwayIndex][sizeIndex];
+        const qty = product.stock.colorway.sizeIndex;
         if (qty > 0) {
             return `${qty} available`;
         }
@@ -27,9 +27,17 @@ export const displayStock = (product: ProductType, colorway: string, size: strin
 
 export const isOutOfStock = (product: ProductType): boolean => {
     // returns true if product is out of stock in all colorways and sizes
-    const totalQty = product.stock.reduce((a,b) => { return a.concat(b) }) // flatten array
-                                    .reduce((a,b) => { return a + b }); // sum elements
-    return (totalQty <= 0);
+    // const totalQty = product.stock.reduce((a,b) => { return a.concat(b) }) // flatten array
+    //                                 .reduce((a,b) => { return a + b }); // sum elements
+    // const totalQty = product.stock.reduce((sum,element) => { return sum + element },0); // sum elements
+    // return (totalQty <= 0);
+    const flatStock = Object.values(product.stock).reduce((acc, stockByColorway)=>{
+        Object.values(stockByColorway).forEach((qty)=>acc.push(qty));
+        return acc
+    }, [] as number[]);
+    const totalQty = flatStock.reduce((acc,qty)=>acc+qty,0);
+    return (totalQty <= 0)
+
 } 
 
 export const isColorwayAvailable = (product: ProductType, colorway: string): boolean => {
@@ -39,12 +47,14 @@ export const isColorwayAvailable = (product: ProductType, colorway: string): boo
     if (index === -1) { // no such colorway
         return false;
     }
-    const colorwayStock = product.stock[index];
-    const totalQty = colorwayStock.reduce((a, b) => {
-        return a + b;
-    }, 0);
-    return (totalQty > 0);
-} 
+    // const colorwayStock = product.stock[index];
+    // const totalQty = colorwayStock.reduce((a, b) => {
+    //     return a + b;
+    // }, 0);
+
+    const colorwayStock = Object.values(product.stock[colorway]).reduce((acc: any, size: any)=>acc+size,0)
+    return (colorwayStock > 0);
+}
 
 export const isSizeAvailable = (product: ProductType, size: string): boolean => {
     // returns true if size is available in any colorway
@@ -53,7 +63,7 @@ export const isSizeAvailable = (product: ProductType, size: string): boolean => 
     if (index === -1) { // no such size
         return false;
     }
-    const sizeStock = product.stock.map(d => d[index]);
+    const sizeStock = Object.values(product.stock).map(d => d[index]);
     const totalQty = sizeStock.reduce((a, b) => {
         return a + b;
     }, 0);
@@ -73,7 +83,7 @@ export const getDefaultColorway = (product: ProductType, size: string): string =
     if (sizeIndex === -1) { // no such size
         return "";
     }
-    const colorwayStock = product.stock.map(d => d[sizeIndex]);
+    const colorwayStock = Object.values(product.stock).map(d => d[sizeIndex]);
     const availColorwayIndex = colorwayStock.map((qty, idx) => qty > 0 ? idx : -1).filter(idx => idx !== -1);
     if (availColorwayIndex.length > 0) {
         return product.colorways[availColorwayIndex[0]];
